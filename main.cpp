fd321208400208824f2afce741eee5d4cf1e104c
Kris <Valentina10@sibmail.com>
2022-11-23 00:13:37 +0700

    Adding int main.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <QCoreApplication>
 
+#include <iostream>
+#include <fstream>
 
 template <typename T>
 
@@ -88,4 +90,29 @@ void Person::setLastName(const std::string& value)
 void Person::setFirstName(const std::string& value)
 {
     first_name = value;
+}
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    PersonKeeper personKeeperInstance;	//создаем экземпляр класса personKeeperInstance
+
+        std::fstream file_in; //создаем объект file_in типа fstream, для работы с файлами. fstream - стандартная библиотека для работы с файлами
+        file_in.open("vvod.txt"); // открываем файл для чтения (или записи, но в данном случае мы будем считывать), используя стандартный метод библиотеки open
+
+        STACK <Person> st;	//создаем стэк из элементов стека класса Person.
+        st = *personKeeperInstance.readPersons(file_in); // метод readPersons возвращает стек с объектами класса Person. При этом мы используем экземпляр класса personKeeperInstance, который мы создали для нашей задачи и сделали там нужные методы... т.е. сделали что-то вроде библиотеки с готовыми методами
+
+        file_in.close(); //закрывает поток file_in чтения из файла
+
+        std::fstream file_out; //создаем объект file_out типа fstream, для работы с файлами
+        file_out.open("vyvod.txt"); //открываем файл для записи
+
+        personKeeperInstance.writePersons(st, file_out); //записываем в файл данные из стека st в файл file_out, при этом используем наш экземпляр класса personKeeperInstance с нужным методом для записи
+
+        file_out.close(); ////закрывает поток file_out
+
+    return a.exec();
 }
